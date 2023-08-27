09dcdd9d6702e2291aee3c4504f8a5211527ac2a
isabeCastillo <139247973+isabeCastillo@users.noreply.github.com>
2023-08-27 15:33:31 -0600

    Add files via upload

---
+++ JuegoX0CP21026.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package juegox0.cp21026;
+
+/**
+ *
+ * @author hp
+ */
+public class JuegoX0CP21026 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
